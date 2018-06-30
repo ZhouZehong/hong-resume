2229f27229e9a90ef0fa14860d91b25b010cf4b1
Hong <714918745@qq.com>
2018-06-30 17:15:14 +0800

    Initial commit

---
+++ src/main/java/com/zzh/hongresume/HongResumeApplication.java
@@ -0,0 +1,12 @@
+package com.zzh.hongresume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HongResumeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HongResumeApplication.class, args);
+    }
+}
---
+++ src/test/java/com/zzh/hongresume/HongResumeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zzh.hongresume;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HongResumeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
